1d62612b77ed582d38ffb6bb2ccb8bbf5f77f033
Ryan Anderson <ryanandersont112@gmail.com>
2025-02-04 22:55:24 -0500

    Started declaration and implementation of the PhysicsBody base class.

--- include/PhysicsBody.hpp
+++ include/PhysicsBody.hpp
@@ -5,9 +5,49 @@
 #ifndef PHYSICS_BODY_HPP
 #define PHYSICS_BODY_HPP
 
+#include "Vector2.hpp"
+
 class PhysicsBody
 {
-    
+private:
+    //Position of body in the world
+    Vector2 position;
+
+    //Current velocity of body (how fast it is moving)
+    Vector2 velocity;
+
+    //Acceleration factor of the body
+    Vector2 acceleration;
+
+    //Mass of the body (weight)
+    float mass;
+
+public:
+    //Constructors and deconstructors
+    PhysicsBody(Vector2 initialPosition = {0, 0},
+    Vector2 initialVelocity = {0, 0},
+    Vector2 initialAcceleration = {0, 0},
+    float mass = 1.0f);
+
+    virtual ~PhysicsBody() = default;
+
+    //Applies a force to the body
+    virtual void applyForce(Vector2 force) = 0;
+
+    //Updates the physics of the body in the world
+    virtual void update(float deltaTime) = 0;
+
+    //Getters for member variables
+    Vector2 getPosition();
+    Vector2 getVelocity();
+    Vector2 getAcceleration();
+    float getMass();
+
+    //Setters for member variables
+    void setPosition(Vector2 newPosition);
+    void setVelocity(Vector2 newVelocity);
+    void setAcceleration(Vector2 newAcceleration);
+    void setMass(float newMass);
 };
 
 #endif
--- src/PhysicsBody.cpp
+++ src/PhysicsBody.cpp
@@ -1,3 +1,52 @@
 //Base class implementation for physics bodies
 
 #include "PhysicsBody.hpp"
+
+//Constructor
+PhysicsBody::PhysicsBody(Vector2 initialPosition, Vector2 initialVelocity, Vector2 initialAcceleration, float mass)
+: position(initialPosition), velocity(initialVelocity), acceleration(initialAcceleration), mass(mass) {}
+
+//Getters for member variables
+Vector2 PhysicsBody::getPosition()
+{
+    return position;
+}
+
+Vector2 PhysicsBody::getVelocity()
+{
+    return velocity;
+}
+
+Vector2 PhysicsBody::getAcceleration()
+{
+    return acceleration;
+}
+
+float PhysicsBody::getMass()
+{
+    return mass;
+}
+
+//Setters for member variables
+void PhysicsBody::setPosition(Vector2 newPosition)
+{
+    position = newPosition;
+}
+
+void PhysicsBody::setVelocity(Vector2 newVelocity)
+{
+    velocity = newVelocity;
+}
+
+void PhysicsBody::setAcceleration(Vector2 newAcceleration)
+{
+    acceleration = newAcceleration;
+}
+
+void PhysicsBody::setMass(float newMass)
+{
+    if (newMass >= 0) //Ensure non-negative mass
+    {
+        mass = newMass;
+    }
+}
