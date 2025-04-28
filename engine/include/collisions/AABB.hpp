ca5afcd64ebe8e54238cf15cc372f320ac517fe3
Ryan Anderson <ryanandersont112@gmail.com>
2025-04-27 21:40:34 -0400

    Delete AABB.cpp and implement in AABB.hpp for simplicity

--- engine/include/collisions/AABB.hpp
+++ engine/include/collisions/AABB.hpp
@@ -14,10 +14,11 @@ struct AABB
     Vector2 max;
 
     //Default constructor
-    AABB();
+    AABB() : min(Vector2()), max(Vector2()) {}
 
     //Constructor to set mins and maxes
-    AABB(const Vector2& min, const Vector2& max);
+    AABB(const Vector2& min, const Vector2& max)
+        : min(min), max(max) {}
 };
 
 #endif
--- engine/src/collisions/AABB.cpp
+++
@@ -1,8 +0,0 @@
-#include "collisions/AABB.hpp"
-
-//Default constructor
-AABB::AABB() : min(Vector2()), max(Vector2()) {}
-
-//Constructor to set mins and maxes
-AABB::AABB(const Vector2& min, const Vector2& max)
-    : min(min), max(max) {}
